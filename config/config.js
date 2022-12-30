5726c004b448d8af8b18b2903b075d1ddf7bcb4b
Roshan <roshan.sharmaa12@gmail.com>
2022-12-30 12:01:23 +0530

    changed server name

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "https://asr-interior-server.herokuapp.com/api/v1";
+export const SERVER_URL = "https://asr-interior-server.onrender.com/api/v1";
 export const CLIENT_URL = "https://asr-interior.com";
