67069aaf15f1ecfc0e3bff098527bf86cd27e4e6
Roshan <roshan.sharmaa12@gmail.com>
2024-08-16 17:03:01 +0530

    Update site url

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const SERVER_URL = "https://asr-interior-server.onrender.com/api/v1";
-export const CLIENT_URL = "https://asr-interior.com";
+// export const CLIENT_URL = "https://asr-interior.com";
+export const CLIENT_URL = "https://asr-interior.netlify.app";
